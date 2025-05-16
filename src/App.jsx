8fc323baba7dbc34fd6f099b690ae24dd14c6b9e
VulpxVenandi25 <emel_d_medina@outlook.com>
2025-05-16 18:15:27 -0500

    Login Estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./Auth/AuthContext";
 import ProtectedRoute from "./Auth/ProtectedRoute";
-
 import Header from "./Components/Header";
-
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Products from "./Pages/Products";
@@ -12,23 +9,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <AuthProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/products"
-              element={
-                <ProtectedRoute>
-                  <Products />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/products"
+            element={
+              <ProtectedRoute>
+                <Products />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
